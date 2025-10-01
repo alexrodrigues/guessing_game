3e3f6d998f06757ca6093c2be2739930c3d2af09
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2025-10-01 17:39:07 -0300

    Enhance StartGameScreen by adding styled TextInput for number input with specific properties; update styles for improved layout and user experience.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,12 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardAppearance="dark"
+        keyboardType="number-pad"
+      />
       <PrimaryButton>Start Game</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -27,5 +32,17 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    alignSelf: "center",
+    fontSize: 32,
+    borderBottomWidth: 2,
+    borderBottomColor: "#ddb52f",
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
